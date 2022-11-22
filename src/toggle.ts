2c663f09a4ec6816ca2cc417bd873bb897511889
Meghea Iulian <iulian.meghea@gmail.com>
2022-11-22 11:13:52 +0200

    feat(toggle): implement dynamic toggle size function
    Allows setting a custom toggle function
    Allows using toggleSize to toggle the width property.

---
+++ src/toggle.ts
@@ -0,0 +1,27 @@
+const defaults: Partial<KeyframeAnimationOptions> = { duration: 250 };
+
+export type SizeP = "width" | "height";
+
+export const toggleSize =
+  (prop: SizeP) =>
+  <T extends HTMLElement>(
+    el: T,
+    visible?: boolean,
+    opts?: Partial<KeyframeAnimationOptions>
+  ) => {
+    const maxSizeProp = "max" + prop.charAt(0).toUpperCase() + prop.slice(1);
+    Object.assign(el.style, { [maxSizeProp]: "", display: "block" });
+    const { [prop]: size } = el.getBoundingClientRect(),
+      kf = [0, size],
+      [from, to] = visible ? kf : kf.slice().reverse(),
+      anim = el.animate(
+        [{ [maxSizeProp]: `${from}px` }, { [maxSizeProp]: `${to}px` }],
+        { ...defaults, ...opts }
+      );
+
+    anim.onfinish = () =>
+      Object.assign(el.style, {
+        [maxSizeProp]: "",
+        display: visible ? "" : "none",
+      });
+  };
