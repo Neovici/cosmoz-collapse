c5738566aafa4c95666b401cb6c91403f6b392e3
Meghea Iulian <iulian.meghea@gmail.com>
2022-11-22 11:25:49 +0200

    chore(ci): upgrade setup

--- src/cosmoz-collapse.ts
+++ src/cosmoz-collapse.ts
@@ -1,28 +1,28 @@
-import { toggleSize } from "./toggle";
+import { toggleSize } from './toggle';
 
 class Collapse extends HTMLElement {
-  static get observedAttributes() {
-    return ["opened"];
-  }
+	static get observedAttributes() {
+		return ['opened'];
+	}
 
-  toggle = toggleSize("height");
+	toggle = toggleSize('height');
 
-  constructor() {
-    super();
-    Object.assign(this.style, { display: "none", overflow: "hidden" });
-  }
+	constructor() {
+		super();
+		Object.assign(this.style, { display: 'none', overflow: 'hidden' });
+	}
 
-  attributeChangedCallback(name: string, prevValue?: unknown, value?: unknown) {
-    switch (name) {
-      case "opened": {
-        const visible = value != null;
-        return this.isConnected
-          ? this.toggle(this, visible)
-          : Object.assign(this.style, { display: visible ? "" : "none" });
-      }
-    }
-  }
+	attributeChangedCallback(name: string, prevValue?: unknown, value?: unknown) {
+		switch (name) {
+			case 'opened': {
+				const visible = value != null;
+				return this.isConnected
+					? this.toggle(this, visible)
+					: Object.assign(this.style, { display: visible ? '' : 'none' });
+			}
+		}
+	}
 }
 
 export { Collapse };
-customElements.define("cosmoz-collapse", Collapse);
+customElements.define('cosmoz-collapse', Collapse);
