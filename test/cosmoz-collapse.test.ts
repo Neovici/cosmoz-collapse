f68d7c4dcb360616b15c04674c153a4d5fd2da91
Husoschi Ionut-Catalin <24230102+ionut@users.noreply.github.com>
2026-06-03 16:01:50 +0300

    chore(deps): update cfg (#10)
    * chore(deps): update cfg
    
    * fix: add test coverage

--- test/cosmoz-collapse.test.ts
+++ test/cosmoz-collapse.test.ts
@@ -1,11 +1,29 @@
+import { aTimeout, expect, fixture, html } from '@open-wc/testing';
 import '../src/cosmoz-collapse';
-import { expect, html, fixture } from '@open-wc/testing';
 
 describe('cosmoz-collapse', () => {
-	it('renders opened', async () => {
-		const el = await fixture(
-			html`<cosmoz-collapse opened><p>Lore ipsum</p></cosmoz-collapse>`,
-		);
-		await expect(el).dom.to.equalSnapshot();
-	});
+  it('renders opened', async () => {
+    const el = await fixture<HTMLElement>(
+      html`<cosmoz-collapse opened><p>Lore ipsum</p></cosmoz-collapse>`,
+    );
+    await expect(el).dom.to.equalSnapshot();
+  });
+
+  it('toggles closed', async () => {
+    const el = await fixture<HTMLElement>(
+      html`<cosmoz-collapse opened><p>Content</p></cosmoz-collapse>`,
+    );
+    el.removeAttribute('opened');
+    await aTimeout(300);
+    expect(el.style.display).to.equal('none');
+  });
+
+  it('toggles open', async () => {
+    const el = await fixture<HTMLElement>(
+      html`<cosmoz-collapse><p>Content</p></cosmoz-collapse>`,
+    );
+    el.setAttribute('opened', '');
+    await aTimeout(300);
+    expect(el.style.display).to.equal('');
+  });
 });
