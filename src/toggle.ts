d0eb06d88f4cefa0fad923cb7aa9dc0e861274f0
ionut <ionuthusoschi@gmail.com>
2026-06-03 12:53:38 +0300

    fix: add overflow to toggle utility function

--- src/toggle.ts
+++ src/toggle.ts
@@ -3,25 +3,30 @@ const defaults: Partial<KeyframeAnimationOptions> = { duration: 250 };
 export type SizeP = 'width' | 'height';
 
 export const toggleSize =
-	(prop: SizeP) =>
-	<T extends HTMLElement>(
-		el: T,
-		visible?: boolean,
-		opts?: Partial<KeyframeAnimationOptions>
-	) => {
-		const maxSizeProp = 'max' + prop.charAt(0).toUpperCase() + prop.slice(1);
-		Object.assign(el.style, { [maxSizeProp]: '', display: '' });
-		const { [prop]: size } = el.getBoundingClientRect(),
-			kf = [0, size],
-			[from, to] = visible ? kf : kf.slice().reverse(),
-			anim = el.animate(
-				[{ [maxSizeProp]: `${from}px` }, { [maxSizeProp]: `${to}px` }],
-				{ ...defaults, ...opts }
-			);
+  (prop: SizeP) =>
+  <T extends HTMLElement>(
+    el: T,
+    visible?: boolean,
+    opts?: Partial<KeyframeAnimationOptions>,
+  ) => {
+    const maxSizeProp = 'max' + prop.charAt(0).toUpperCase() + prop.slice(1);
+    Object.assign(el.style, {
+      [maxSizeProp]: '',
+      display: '',
+      overflow: 'hidden',
+    });
+    const { [prop]: size } = el.getBoundingClientRect(),
+      kf = [0, size],
+      [from, to] = visible ? kf : kf.slice().reverse(),
+      anim = el.animate(
+        [{ [maxSizeProp]: `${from}px` }, { [maxSizeProp]: `${to}px` }],
+        { ...defaults, ...opts },
+      );
 
-		anim.onfinish = () =>
-			Object.assign(el.style, {
-				[maxSizeProp]: '',
-				display: visible ? '' : 'none',
-			});
-	};
+    anim.onfinish = () =>
+      Object.assign(el.style, {
+        [maxSizeProp]: '',
+        display: visible ? '' : 'none',
+        overflow: visible ? '' : 'visible',
+      });
+  };
